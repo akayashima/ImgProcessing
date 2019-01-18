c920e040583191526863a5fc6ab364808af2dd42
akayashima <b153k061@is.kochi-u.ac.jp>
2019-01-18 20:31:32 +0900

    MicroBlazei0 and MicroBlazei1
    縦横1ピクセルがおかしい

--- Mutex-20181213/Multiple-Core.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/debug.tcl
+++ Mutex-20181213/Multiple-Core.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/debug.tcl
@@ -1,6 +1,4 @@
 connect -url tcp:127.0.0.1:3121
-targets -set -filter {jtag_cable_name =~ "Digilent Nexys Video 210276689540B" && level==0} -index 0
-fpga -file /home/akayashima/H30_Xilinx/ImgProcessing/Mutex-20181213/Multiple-Core.sdk/design_1_wrapper_hw_platform_0/design_1_wrapper.bit
 targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys Video 210276689540B"} -index 0
 rst -system
 after 3000
